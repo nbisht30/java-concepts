afe9963815ccaafa0ba4df4e9b378c12a27fe869
nbisht30 <nbisht30@gmail.com>
2020-01-22 00:48:31 +0530

    Lambdas

--- src/javaeight/ImperativeVsDeclarativeExample2.java
+++ src/javaeight/ImperativeVsDeclarativeExample2.java
@@ -6,7 +6,7 @@ import java.util.stream.IntStream;
  * @author Nikhil Bisht
  * @date 07-01-2020
  */
-public class ImperativeVsDeclarativeExample1 {
+public class ImperativeVsDeclarativeExample2 {
     public static void main(String[] args) {
 //        Imperative Style Of Programming
 
--- src/pluralsight/lambdaexpressions/Lambda.java
+++ src/pluralsight/lambdaexpressions/Lambda.java
@@ -1,6 +1,8 @@
 package pluralsight.lambdaexpressions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -17,5 +19,13 @@ public class Lambda {
 
         //forEach() of java 8
         items.forEach(listItem -> System.out.println(listItem * listItem));  //print sq. of items
+
+        String[] words = {"Bye", "Hi", "Adios", "Hola", "Aloha"};
+
+        Arrays.sort(words, (s1, s2) -> Integer.compare(s1.length(), s2.length()));
+        Arrays.stream(words).forEach(System.out::println);
+        System.out.println(words);
+        //OR
+        Arrays.sort(words, Comparator.comparingInt(String::length));
     }
 }
