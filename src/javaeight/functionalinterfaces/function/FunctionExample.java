dd72d1bf35bdcdafc970c9c962ee2232dd5740ba
nbisht30 <nbisht30@gmail.com>
2020-02-14 10:55:21 +0530

    Java 8 Functions

--- src/javaeight/functionalinterfaces/function/FunctionExample.java
+++ src/javaeight/functionalinterfaces/function/FunctionExample.java
@@ -1,8 +1,19 @@
 package javaeight.functionalinterfaces.function;
 
+import java.util.function.Function;
+
 /**
  * @author Nikhil Bisht
  * @date 27-01-2020
  */
 public class FunctionExample {
+    static Function<String, String> function = name -> name.toUpperCase();
+    static Function<String, String> functionAndThen = name -> name.concat("Concatenated");
+
+
+    public static void main(String[] args) {
+        System.out.println("Result is : " + function.andThen(functionAndThen).apply("java 8"));
+        System.out.println("Result is : " + function.compose(functionAndThen).apply("java 8")); //performs functionAndThen first
+        //and passes the result to function
+    }
 }
---
+++ src/javaeight/functionalinterfaces/function/FunctionRealWorldExample.java
@@ -0,0 +1,29 @@
+package javaeight.functionalinterfaces.function;
+
+import javaeight.functionalinterfaces.consumer.realworldexample.Student;
+import javaeight.functionalinterfaces.consumer.realworldexample.StudentDataBase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * @author Nikhil Bisht
+ * @date 27-01-2020
+ */
+public class FunctionRealWorldExample {
+    //a function that takes list of students as input and returns a map fo student name and their gpas
+    static Function<List<Student>, Map<String, Double>> studentFunction =
+            students -> {
+                Map<String, Double> studentGradeMap = new HashMap<>();
+                students.forEach(student -> {
+                    studentGradeMap.put(student.getName(), student.getGpa());
+                });
+                return studentGradeMap;
+            };
+
+    public static void main(String[] args) {
+        System.out.println(studentFunction.apply(StudentDataBase.getAllStudents()));
+    }
+}
