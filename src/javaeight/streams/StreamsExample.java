fde6c2df3932f457aa2d1beecf7c4b3fb9530b58
nbisht30 <nbisht30@gmail.com>
2020-02-18 08:48:35 +0530

    Debugging streams

--- src/javaeight/streams/StreamsExample.java
+++ src/javaeight/streams/StreamsExample.java
@@ -22,7 +22,7 @@ public class StreamsExample {
 
         //Just get students with certain conditions
         Predicate<Student> gradePred = student -> student.getGradeLevel() >= 3;
-        Predicate<Student> gpaPred = student -> student.getGpa() >= 3;
+        Predicate<Student> gpaPred = student -> student.getGpa() >= 3.9;
         Map<String, List<String>> studentAndActivitiesGradeMoreThan3 = StudentDataBase.getAllStudents().stream().
                 filter(gradePred).  //filter() for a condition
                 filter(gpaPred).  //filter() for a condition
@@ -33,7 +33,23 @@ public class StreamsExample {
         //Streams can be traversed only once.
         Stream<Student> streamOfStudents = StudentDataBase.getAllStudents().stream();
         streamOfStudents.forEach(student -> System.out.println(student.getName()));
-        streamOfStudents.forEach(student -> System.out.println(student.getName())); //Exception in thread "main" java.lang.IllegalStateException:
+//        streamOfStudents.forEach(student -> System.out.println(student.getName())); //Exception in thread "main" java.lang.IllegalStateException:
         // stream has already been operated upon or closed
+        System.out.println("=================================");
+
+        //Debugging stream operations
+        Map<String, List<String>> studentMapForDebugging = StudentDataBase.getAllStudents().stream().
+                peek(System.out::println).
+                filter(gradePred).  //filter() for a condition
+                peek(student -> {
+            System.out.println("After 1st filter:"+ student) ;
+        }).
+                filter(gpaPred).  //filter() for a condition
+                peek(student -> {
+            System.out.println("After 2nd filter:"+student);
+        }).
+                collect(Collectors.toMap(Student::getName, Student::getActivities));
+        studentAndActivitiesGradeMoreThan3.forEach((name, activities) -> System.out.println(name + " : " + activities));
+
     }
 }
